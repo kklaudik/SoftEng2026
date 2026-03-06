93b7f5065ddd89a5adef030ec38c87050f9fd34b
Volt1sr25 <bartekbanaszek8@gmail.com>
2026-03-06 18:55:58 +0100

    adding circle

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -10,3 +10,13 @@ this->area=static_cast<T>(PI*radius*radius);
 this->perimeter=static_cast<T>(2*PI*radius);
 }
 
+template <class T> void Circle<T>::print() {  
+    T radius = this->param->get(PARAM_RADIUS); 
+    std::cout << "=== FIGURA: KOLO ===" << std::endl; 
+    std::cout << "Promien: " << radius << std::endl; 
+    std::cout << "Pole powierzchni: " << this->area << std::endl; 
+    std::cout << "Obwod: " << this->perimeter << std::endl; 
+    std::cout << "====================" << std::endl; 
+} 
+
+
