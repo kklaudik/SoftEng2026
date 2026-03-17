6b400fb6e1fd5913dbbcdf879a9d5233d5e6b07c
Szafraniec <marcel.szafraniec@gmail.com>
2026-03-17 10:50:58 +0100

    Poprawa Hexagon.v2

--- src/Hexagon.h
+++ src/Hexagon.h
@@ -10,40 +10,30 @@ using namespace std;
 #include "ShapeParam.h"
 #include "ShapeParamIndex.h"
 
-template<class T>
-class Hexagon : public Shape2D<T> {
+template <class T> class Hexagon : public Shape2D<T> {
 public:
     inline ShapeResult<T> compute();
-
     inline string print();
-
     inline Hexagon(const ShapeParam<T>& param);
-
 };
 
-template<class T>
-inline ShapeResult<T> Hexagon<T>::compute() {
+template <class T> inline ShapeResult<T> Hexagon<T>::compute()
+{
 
     ShapeResult<T> result;
-
     T a = this->m_param.get_attrib(PARAM_RADIUS);
-
     T area = (3 * sqrt(3) * a * a) / 2;
     T perimeter = 6 * a;
-
     result.set_attrib(RESULT_AREA, area);
     result.set_attrib(RESULT_PERIMETER, perimeter);
 
     return result;
 }
 
-template<class T>
-inline string Hexagon<T>::print() {
-    return "Hexagon";
-}
+template <class T> inline string Hexagon<T>::print() { return "Hexagon"; }
 
-template<class T>
-inline Hexagon<T>::Hexagon(const ShapeParam<T>& param) : Shape2D<T>(param) {
-}
+template <class T>
+inline Hexagon<T>::Hexagon(const ShapeParam<T>& param): Shape2D<T>(param)
+{}
 
 #endif
