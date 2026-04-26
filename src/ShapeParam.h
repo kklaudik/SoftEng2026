60812acb780e9e80085ec355a89001f096048fe4
Klaudia <krysinskaklaudia9@gmail.com>
2026-04-26 12:16:58 +0200

    Update rhombus validation_KKKB

--- src/ShapeParam.h
+++ src/ShapeParam.h
@@ -7,38 +7,44 @@ using namespace std;
 #include "ShapeType.h"
 #include "ShapeParamIndex.h"
 
-template<class T>
-class ShapeParam {
-  private:
+template <class T> class ShapeParam {
+private:
     vector<T> attribs;
 
 
-  public:
+public:
     ShapeType type;
 
     inline T get_attrib(ShapeParamIndex ind) const;
 
-    inline bool set_attrib(ShapeParamIndex ind, const T & val);
+    inline bool set_attrib(ShapeParamIndex ind, const T& val);
 
     inline bool validate() const;
-
 };
-template<class T>
-inline T ShapeParam<T>::get_attrib(ShapeParamIndex ind) const {
-    if(ind<attribs.size()) return attribs[ind];
+template <class T> inline T ShapeParam<T>::get_attrib(ShapeParamIndex ind) const
+{
+    if (ind < attribs.size()) return attribs[ind];
     return 0;
 }
 
-template<class T>
-inline bool ShapeParam<T>::set_attrib(ShapeParamIndex ind, const T & val) {
-  if(ind>=attribs.size())attribs.resize(ind+1);
-  attribs[ind]=val;
-  return true;
+template <class T>
+inline bool ShapeParam<T>::set_attrib(ShapeParamIndex ind, const T& val)
+{
+    if (ind >= attribs.size()) attribs.resize(ind + 1);
+    attribs[ind] = val;
+    return true;
 }
 
-template<class T>
-inline bool ShapeParam<T>::validate() const {
-  return true;
+template <class T> inline bool ShapeParam<T>::validate() const
+{
+    switch (type)
+    {
+        case PT_RHOMBUS:
+            return attribs.size() > PARAM_DIAGONAL_2
+                && attribs[PARAM_DIAGONAL] > 0 && attribs[PARAM_DIAGONAL_2] > 0;
+
+        default: return true;
+    }
 }
 
 #endif
